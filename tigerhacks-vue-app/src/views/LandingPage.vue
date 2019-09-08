5c1b2e42a98737d93dbb31747bca9f6903ce4691
Michael Woodruff <michaelwoodruff@Michaels-MacBook-Pro.local>
2019-09-08 11:14:10 -0500

    add vuetify to project

---
+++ tigerhacks-vue-app/src/views/LandingPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <md-list>
+    <md-list-item>
+      <md-icon>Hello?</md-icon>
+      <span class="md-list-item-text">Inbox</span>
+    </md-list-item>
+  </md-list>
+</template>
+
+<script>
+import VueMaterial from "vue-material";
+import Vue from "vue";
+
+Vue.use(VueMaterial);
+
+export default {
+  name: "LandingPage",
+  data() {
+    return {};
+  },
+  props: {}
+};
+</script>
+
+<style>
+</style>
